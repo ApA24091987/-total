527ae839c2f33d4a905aea094a7a6831cfa8588e
ApA24091987 <168029434+ApA24091987@users.noreply.github.com>
2024-05-30 13:35:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from fake_m import divide as fake_values
+from true_m import divide as true_values
+result1 = fake_values(69, 3)
+result2 = fake_values(3, 0)
+result3 = true_values(49, 7)
+result4 = true_values(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
